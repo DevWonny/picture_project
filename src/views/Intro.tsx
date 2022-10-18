0a2639240ac8bb322f68bf60eb06e181f7abb51e
DevWonny <cjfdnjs1994@naver.com>
2022-10-18 13:12:14 +0900

    feat : intro page 퍼블리싱 구현 중

--- src/views/Intro.tsx
+++ src/views/Intro.tsx
@@ -1,5 +1,38 @@
 import React from "react";
+import styled from "styled-components";
 
-const Intro = () => {};
+const Intro = () => {
+  return (
+    <IntroWrap>
+      <span>test</span>
+
+      <IntroButton sign={false}>Login</IntroButton>
+      <IntroButton sign={true}>Sign Up</IntroButton>
+    </IntroWrap>
+  );
+};
 
 export default Intro;
+
+const IntroWrap = styled.div`
+  max-width: 450px;
+  min-height: 900px;
+  background: #8b7c67;
+  position: relative;
+`;
+
+const IntroButton = styled.div<{ sign: boolean }>`
+  width: 100px;
+  height: 50px;
+  border-radius: 10px;
+  text-align: center;
+  line-height: 50px;
+  background-color: #cabfae;
+  color: #767971;
+  font-size: 20px;
+  position: absolute;
+  bottom: 130px;
+  left: ${(props) => !props.sign && "75px"};
+  right: ${(props) => props.sign && "75px"};
+  cursor: pointer;
+`;
